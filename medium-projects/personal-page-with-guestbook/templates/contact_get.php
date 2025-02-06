b0c86b1fc9f41badf2d7e87c21e2f142eaa1377d
gdbi7 <gonzalobrandan@outlook.de>
2025-02-06 12:57:25 +0100

    add public note/question form

---
+++ medium-projects/personal-page-with-guestbook/templates/contact_get.php
@@ -0,0 +1,13 @@
+<section>
+    <h2>Leave a public note/question</h2>
+    <form method="POST">
+    <!-- CSRF -->
+    <label for="">Name</label>
+    <input type="text" name="name" />
+    <label for="">Email</label>
+    <input type="email" />
+    <label for="">Message</label>
+    <textarea rows="4" name="message"></textarea>
+    <button type="submit">Send Message</button>
+    </form>
+</section>
