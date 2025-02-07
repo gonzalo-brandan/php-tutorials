d17e23693092fc8dca02655904cd1141994a369a
gdbi7 <gonzalobrandan@outlook.de>
2025-02-07 16:18:26 +0100

    fix CSRF token validation and regeneration

--- medium-projects/personal-page-with-guestbook/includes/csrf.php
+++ medium-projects/personal-page-with-guestbook/includes/csrf.php
@@ -2,44 +2,62 @@
 declare(strict_types=1);
 
 const CSRF_TOKEN_LENGTH = 32;
-const CSRF_TOKEN_LIFETIME = 60 * 30; //30min
+const CSRF_TOKEN_LIFETIME = 60 * 30; // 30 minutes
 
-function generateCsrfToken(): string{
-    $token = bin2hex(random_bytes(CSRF_TOKEN_LENGTH));
-    setCsrfTokenAndTime($token);
-    return $token;
+function generateCsrfToken(): string {
+  $token = bin2hex(random_bytes(CSRF_TOKEN_LENGTH));
+  setCsrfTokenAndTime($token);
+  return $token;
 }
 
-function getCsrfTokenAndTime(): array{
-    return [
-        $_SESSION['csrf_token'] ?? null,
-        $_SESSION['csrf_token_time'] ?? null,
-    ];
+function getCsrfTokenAndTime(): array {
+  return [
+    $_SESSION['csrf_token'] ?? null,
+    $_SESSION['csrf_token_time'] ?? null,
+  ];
 }
 
-function setCsrfTokenAndTime(string $token): void{
-    $_SESSION['csrf_token'] = $token;
-    $_SESSION['csrf_token_time'] = time();
+function setCsrfTokenAndTime(?string $token): void {
+  if ($token === null) {
+    unset(
+      $_SESSION['csrf_token'],
+      $_SESSION['csrf_token_time']
+    );
+    return;
+  }
+  $_SESSION['csrf_token'] = $token;
+  $_SESSION['csrf_token_time'] = time();
 }
 
 function isTokenExpired(?int $time): bool {
-    return $time === null || (time() - $time) > CSRF_TOKEN_LIFETIME;
+  return $time === null || (time() - $time) > CSRF_TOKEN_LIFETIME;
 }
 
-function getCurrentCsrfToken(): string{
-    //1. check if there is a valid non expired token already
-    //return if exist
-    //2. generate a new one
-    //return it
-    [$token, $time] = getCsrfTokenAndTime();
-    if(!isset($token, $time) || isTokenExpired($time)){
-        return generateCsrfToken();
-    }
-    return $_SESSION['csrf_token'];
+function getCurrentCsrfToken(): string {
+  // 1. To check if there is a valid, non-expired token already
+  //    return if it exists
+  // 2. Generate a new one
+  //    return it
+  [$token, $time] = getCsrfTokenAndTime();
+  if (!isset($token, $time) || isTokenExpired($time)) {
+    return generateCsrfToken();
+  }
+  return $_SESSION['csrf_token'];
 }
 
-var_dump(generateCsrfToken());die;
-
-function validateCsrfToken(?string $token): bool{
+function validateCsrfToken(?string $token): bool {
+  [$storedToken, $time] = getCsrfTokenAndTime();
+  if (!isset($storedToken, $time)) {
     return false;
+  }
+  if (isTokenExpired($time)) {
+    setCsrfTokenAndTime(null);
+    return false;
+  }
+  // Validate the token
+  $valid = hash_equals($storedToken, $token ?? '');
+  if ($valid) {
+    generateCsrfToken();
+  }
+  return $valid;
 }
