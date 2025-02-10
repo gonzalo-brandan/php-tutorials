7558a641fe2d4ef39d4e5d2a3c2a3ac8812edd90
gdbi7 <gonzalobrandan@outlook.de>
2025-02-10 14:30:20 +0100

    define routes in routes.php and integrate with Router in index.php

--- medium-projects/blog-oop-and-custom-framework/core/Router.php
+++ medium-projects/blog-oop-and-custom-framework/core/Router.php
@@ -3,67 +3,68 @@
 namespace Core;
 
 class Router {
-    protected array $routes = [];
+  protected array $routes = [];
 
-    public function add(string $method, string $uri, string $controller): void {
-        $this->routes[] = [
-            'method' => $method,
-            'uri' => $uri,
-            'controller' => $controller
-        ];
+  public function add(string $method, string $uri, string $controller): void {
+    $this->routes[] = [
+      'method' => $method,
+      'uri' => $uri,
+      'controller' => $controller
+    ];
+  }
+
+  public function notFound(): void {
+    http_response_code(404);
+    echo "404 Not Found";
+    exit;
+  }
+
+  public function dispatch(string $uri, string $method): string {
+    $route = $this->findRoute($uri, $method);
+
+    if (!$route) {
+      return $this->notFound();
+    }
+    // PostController@index
+    // PostController@show
+    [$controller, $action] = explode('@', $route['controller']);
+    return $this->callAction($controller, $action, $route['params']);
+  }
+
+  protected function findRoute(string $uri, string $method): ?array {
+    foreach ($this->routes as $route) {
+      $params = $this->matchRoute($route['uri'], $uri);
+      if ($params !== null && $route['method'] === $method) {
+        return [...$route, 'params' => $params];
+      }
     }
 
-    public function notFound(): void{
-        http_response_code(404);
-        echo "404 not found";
-        exit;
+    return null;
+  }
+
+  protected function matchRoute(string $routeUri, string $requestUri): ?array {
+    $routeSegments = explode('/', trim($routeUri, '/'));
+    $requestSegments = explode('/', trim($requestUri, '/'));
+
+    if (count($routeSegments) !== count($requestSegments)) {
+      return null;
     }
 
-    public function dispatch(string $uri, string $method): string {
-        $route = $this->findRoute($uri, $method);
+    $params = [];
 
-        if(!$route){
-            return $this->notFound();
-        }
-
-        //PostController@index
-        //PostController@show
-        [$controller, $action] = explode('@', $route['controller']);
-        return $this->callAction($controller, $action, $route['params']);
+    foreach ($routeSegments as $index => $routeSegment) {
+      if (str_starts_with($routeSegment, '{') && str_ends_with($routeSegment, '}')) {
+        $params[trim($routeSegment, '{}')] = $requestSegments[$index];
+      } elseif($routeSegment !== $requestSegments[$index]) {
+        return null;
+      }
     }
 
-    protected function findRoute(string $uri, string $method): ?array {
-        foreach($this->routes as $route){
-            $params = $this->matchRoute($route['uri'], $uri);
-            if($params != null && $route['method'] === $method){
-                return [...$route, 'params' => $params];
-            }
-        }
-    }
+    return $params;
+  }
 
-    protected function matchRoute(string $routeUri, string $requestUri): ?array{
-        $routeSegments = explode('/', trim($routeUri, '/'));
-        $requestSegments = explode('/', trim($requestUri, '/'));
-        
-        if(count($routeSegments) != count($requestSegments)){
-            return null;
-        }
-
-        $params = [];
-
-        foreach($routeSegments as $index => $routeSegment){
-            if(str_starts_with($routeSegment, '{') && str_ends_with($routeSegment, '}')) {
-                $params[trim($routeSegment, '{}')] = $requestSegments[$index];
-            } elseif($routeSegment != $requestSegments[$index]){
-                return null;
-            }
-        }
-
-        return $params;
-    }
-
-    protected function callAction(string $controller, string $action, array $params): string{
-        $controllerClass = "App\\Controller\\$controller";
-        return (new $controllerClass)->$action(...$params);
-    }
+  protected function callAction(string $controller, string $action, array $params): string {
+    $controllerClass = "App\\Controller\\$controller";
+    return (new $controllerClass)->$action(...$params);
+  }
 }
--- medium-projects/blog-oop-and-custom-framework/public/index.php
+++ medium-projects/blog-oop-and-custom-framework/public/index.php
@@ -6,3 +6,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Core\Router;
 
 $router = new Router();
+require_once __DIR__ . '/../routes.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+</head>
+<body>
+    <?php echo $router->dispatch($uri, $method); ?>
+</body>
+</html>
