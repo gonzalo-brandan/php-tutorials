e80571f354b6716f07e0c6a1d38ad03e308ebaff
gdbi7 <gonzalobrandan@outlook.de>
2025-02-10 14:20:43 +0100

    place controllers in the right path

---
+++ medium-projects/blog-oop-and-custom-framework/app/Controller/PostController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+class PostController {
+  public function index() {
+    return "All posts";
+  }
+  public function show($id) {
+    return "Post nr $id";
+  }
+}
