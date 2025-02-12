22906fb852f7ec4c4f9b94441722868db877a87d
gdbi7 <gonzalobrandan@outlook.de>
2025-02-12 11:43:36 +0100

    add application bootstrap, config, and service container

--- medium-projects/blog-oop-and-custom-framework/public/index.php
+++ medium-projects/blog-oop-and-custom-framework/public/index.php
@@ -1,9 +1,9 @@
 <?php
 declare (strict_types = 1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
 
-use Core\Router;
+  use Core\Router;
 
 $router = new Router();
 require_once __DIR__ . '/../routes.php';
