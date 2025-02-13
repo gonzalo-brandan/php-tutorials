feb622a5f7451596029685bc95d42c77ea41fee9
gdbi7 <gonzalobrandan@outlook.de>
2025-02-13 16:26:26 +0100

    implement template partials

--- medium-projects/blog-oop-and-custom-framework/public/index.php
+++ medium-projects/blog-oop-and-custom-framework/public/index.php
@@ -3,10 +3,12 @@ declare (strict_types = 1);
 
 require_once __DIR__ . '/../bootstrap.php';
 
-  use Core\Router;
+use Core\Router;
 
 $router = new Router();
 require_once __DIR__ . '/../routes.php';
+require_once __DIR__ . '/../helpers.php';
+
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_SERVER['REQUEST_METHOD'];
