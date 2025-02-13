fb4f30dc47d1fc9ae9ee61f09b3f4cd6db3e833f
gdbi7 <gonzalobrandan@outlook.de>
2025-02-13 15:19:15 +0100

    add post details page with comments and view count tracking

--- medium-projects/blog-oop-and-custom-framework/app/Controllers/PostController.php
+++ medium-projects/blog-oop-and-custom-framework/app/Controllers/PostController.php
@@ -1,10 +1,32 @@
 <?php
 namespace App\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Core\Router;
+use Core\View;
+
 class PostController {
   public function index() {
     return "All posts";
   }
   public function show($id) {
-    return "Post nr $id";
+    //1. fetch
+    $post = Post::find($id);
+    //2. 404
+    if(!$post){
+      Router::notFound();
+    }
+    //3. load comments
+    $comments = Comment::forPost($id);
+    //4. Increment view number
+    Post::incrementViews($id);
+    //5. render
+    return View::render(
+      template: 'post/show',
+      layout: 'layouts/main',
+      data: ['post' => $post, 'comments' => $comments]
+    );
+
   }
 }
